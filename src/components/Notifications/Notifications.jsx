60657c199545610541886846020a63a828d18341
direct66612 <direct66612@gmail.com>
2023-11-16 15:56:37 -0500

    update

--- src/components/Notifications/Notifications.jsx
+++
@@ -1,7 +0,0 @@
-import { Component } from 'react';
-class Notifications extends Component {
-  render() {
-    return <>{this.props.message}</>;
-  }
-}
-export default Notifications;
--- src/components/Section/Section.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-const Title = styled('h2')`
-  font-size: 36px;
-`;
-const Container = styled('div')`
-  text-align: center;
-`;
-export { Title, Container };
